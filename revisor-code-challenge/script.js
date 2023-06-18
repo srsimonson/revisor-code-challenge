a740225d578ae047193e555dca219a702014cbc0
Steve Simonson <srsimonson@gmail.com>
2023-06-18 16:53:18 -0500

    revisor/ add red dot validation error on name input

--- revisor-code-challenge/script.js
+++ revisor-code-challenge/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+console.log("Hello World");
+
+function submitForm() {
+    console.log("submitForm fired");
+}
+
+const nameInput = document.getElementById("name");
+
+
+nameInput.addEventListener("input", () => {
+    const errorDot = document.querySelector('.error-dot');
+
+    if ( nameInput.validity.patternMismatch || nameInput.value.length === nameInput.maxLength) {
+        errorDot.style.display = 'inline';
+    } else {
+        errorDot.style.display = 'none';
+    }
+});
+
